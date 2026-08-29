bdf512cd5b8cb6f731843f475f45a6369cccfcb0
rbonuccelli73 <rbonuccelli07@gmail.com>
2026-08-29 13:28:52 -0300

    Segunda versão do aplicativo

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
-    byte idade = 15;
+    byte idade = 18;
 
     if (idade <= 10) {
         System.out.println("Criança");
     } else if (idade <= 17) {
         System.out.println("Adolescente");
+    }else{
+        System.out.println("Adulto");
     }
 }
