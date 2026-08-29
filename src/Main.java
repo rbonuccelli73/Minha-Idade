bf9b14ae591318cefcc648ab2d74785f24dc94c9
rbonuccelli73 <rbonuccelli07@gmail.com>
2026-08-29 13:22:04 -0300

    Primeira versão do aplicativo

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    byte idade = 15;
+
+    if (idade <= 10) {
+        System.out.println("Criança");
+    } else if (idade <= 17) {
+        System.out.println("Adolescente");
+    }
+}
